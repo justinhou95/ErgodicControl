4f997d635bbc112beaa74df56911833ecdbbf728
Songyan Hou <1819475280@qq.com>
2020-06-11 17:04:47 +0800

    BM&MR

--- Archiv/FHmodelMR.py
+++ Archiv/FHmodelMR.py
@@ -4,20 +4,14 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-# Control Neural Network: u(x)\
-def control_NN_initial():   
-    inputs = keras.Input(shape=(1))
-    l1 = layers.Dense(8, activation = 'elu')
-    l2 = layers.Dense(8, activation = 'elu')
-    l3= layers.Dense(1, activation = 'linear')
-    outputs = l1(inputs)
-    outputs = l2(outputs)
-    outputs = l3(outputs)
-    control_NN = keras.Model(inputs=inputs, outputs=outputs, name = 'control_NN')
-    # control_NN.summary()
-    # print('control_NN')
-    # NN_plot(control_NN)
-    return control_NN
+a = 2*(np.sqrt(2) - 1)
+b = 2 - np.sqrt(2)
+
+def u_optimal(x):
+    return -0.5*(a*x + b)
+
+def MR(u,x):
+    return (u + 1 -x)
 
 
 def trainning_data(seed,X0,samplesM,stepsN,dtt):
@@ -43,23 +37,6 @@ class MODEL:
         self.sqrtdt = np.sqrt(self.dt)
         self.T = self.steps * self.dt
         self.M = M
-    def dyna(self,name = 'bm'):
-        if name == 'bm':
-            def u_optimal(x):
-                return -x
-            self.u_optimal = u_optimal
-            def dynamic(u,x):
-                return u
-            self.dynamic = dynamic
-        if name == 'mr':
-            def u_optimal(x):
-                a = 2*(np.sqrt(2) - 1)
-                b = 2 - np.sqrt(2)
-                return -0.5*(a*x + b)
-            self.u_optimal = u_optimal
-            def dynamic(u,x):
-                return (u + 1 -x)  
-            self.dynamic = dynamic
     def nn(self,unn):
         self.unn = unn
     def start(self,X0):
@@ -79,8 +56,8 @@ class MODEL:
         for i in range(self.steps):
             input_dW = keras.Input(shape=(1))
             inputs = inputs + [input_dW]
-            u_now = self.u_optimal(X_now)  # Here we use the optimal control
-            X_next  = X_now + input_dW + self.dynamic(u_now,X_now) * self.dt
+            u_now = u_optimal(X_now)              # Here we use the optimal control
+            X_next  = X_now + input_dW + MR(u_now, X_now) * self.dt     # Dynamics of MR
             loss_tmp = (tf.math.square(X_now) + tf.math.square(u_now))*self.dt
             loss_output = loss_output + [loss_tmp]
             loss = loss + loss_tmp
@@ -101,7 +78,7 @@ class MODEL:
             inputs = inputs + [input_dW]
             u_now = self.unn(X_now)
         #     u_now = -0.5*d_value_NN(X_now)    #  Connect u with dV/dx 
-            X_next  = X_now + input_dW + self.dynamic(u_now,X_now) * self.dt
+            X_next  = X_now + input_dW + MR(u_now, X_now) * self.dt  
             loss_tmp = (tf.math.square(X_now) + tf.math.square(u_now))*self.dt
             loss_output = loss_output + [loss_tmp]
             loss = loss + loss_tmp
@@ -113,7 +90,7 @@ class MODEL:
     def plot_compare(self):
         xgrid = np.linspace(-10,10,201)
         NN_plot(self.unn)
-        plt.plot(xgrid, self.u_optimal(xgrid))
+        plt.plot(xgrid, u_optimal(xgrid))
         plt.show()
     def end(self):
         self.endnn.compile(loss = 'mse')
--- MBSDEmodel.py
+++ MBSDEmodel.py
@@ -4,6 +4,53 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+def initial_control_NN():
+    # Control Neural Network: u(x)
+    inputs = keras.Input(shape=(1))
+    l1 = layers.Dense(8, activation = 'elu')
+    l2 = layers.Dense(8, activation = 'elu')
+    l3= layers.Dense(1, activation = 'linear')
+    outputs = l1(inputs)
+    outputs = l2(outputs)
+    outputs = l3(outputs)
+    control_NN = keras.Model(inputs=inputs, outputs=outputs, name = 'control_NN')
+    # control_NN.summary()
+#     NN_plot(control_NN)
+#     plt.title('control NN')
+#     plt.show()
+    return control_NN
+
+def initial_value_NN():
+    # Value Neural Network: V(x)
+    inputs = keras.Input(shape=(1))
+    inputs0 = tf.zeros_like(inputs)
+    l1 = layers.Dense(8, activation = 'elu')
+    l2= layers.Dense(8, activation = 'elu')
+    l3= layers.Dense(1, activation = 'linear')
+    outputs = l1(inputs)
+    outputs = l2(outputs)
+    outputs = l3(outputs)
+    outputs0 = l1(inputs0)
+    outputs0 = l2(outputs0)
+    outputs0 = l3(outputs0)
+    outputs = outputs - outputs0
+    value_NN = keras.Model(inputs=inputs, outputs=outputs, name = 'value_NN')
+    # value_NN.summary()
+#     NN_plot(value_NN)
+#     plt.title('value NN')
+#     plt.show()
+    return value_NN
+
+def initial_rho_NN():
+    # optimal value: rho
+    inputs = keras.Input(shape=(1))
+    l= layers.Dense(1, activation = 'linear')
+    outputs = l(inputs)
+    rho_NN = keras.Model(inputs=inputs, outputs=outputs, name = 'rho')
+    # rho_NN.summary()
+#     print('rho_NN: ',rho_NN.predict(np.zeros(1))[0,0])
+    return rho_NN
+
 def square_loss(target_y, predicted_y):
     return tf.reduce_mean(tf.math.square(predicted_y - target_y))
 custom_optimizer = keras.optimizers.Adam(learning_rate = 0.01)
@@ -31,6 +78,33 @@ class BSDE:
         self.sqrtdt = np.sqrt(self.dt)
         self.T = self.steps * self.dt
         self.M = M
+    def dyna(self,name = 'bm'):
+        if name == 'bm':
+            def u_optimal(x):
+                return -x
+            self.u_optimal = u_optimal
+            def dynamic(u,x):
+                return u
+            self.dynamic = dynamic
+            def value_optimal(x):
+                return x**2
+            self.value_optimal = value_optimal
+            self.rho_optimal = 1
+        if name == 'mr':
+            def u_optimal(x):
+                a = 2*(np.sqrt(2) - 1)
+                b = 2 - np.sqrt(2)
+                return -0.5*(a*x + b)
+            self.u_optimal = u_optimal
+            def dynamic(u,x):
+                return (u + 1 -x)  
+            self.dynamic = dynamic
+            def value_optimal(x):
+                a = 2*(np.sqrt(2) - 1)
+                b = 2 - np.sqrt(2)
+                return a*x**2/2 + b*x
+            self.value_optimal = value_optimal
+            self.rho_optimal = (2*np.sqrt(2) - 1)/2
     def nn(self,unn,Vnn,rhonn):
         self.unn = unn
         self.Vnn = Vnn
@@ -53,15 +127,16 @@ class BSDE:
         for i in range(self.steps):
             input_dW = keras.Input(shape=(1))
             inputs = inputs + [input_dW]
-            u_now = -X_now              # Here we use the optimal control
-            X_next  = X_now + input_dW + u_now * self.dt
+            u_now = self.u_optimal(X_now)              # Here we use the optimal control
+            X_next  = X_now + input_dW + self.dynamic(u_now,X_now) * self.dt
             loss_tmp = (tf.math.square(X_now) + tf.math.square(u_now))*self.dt
             loss_output = loss_output + [loss_tmp]
             loss = loss + loss_tmp
             bsde_tmp = -2*tf.multiply(u_now,input_dW)
             bsde = bsde + bsde_tmp
             X_now = X_next
-        outputs = loss - bsde - self.T + tf.math.square(X_now) - tf.math.square(X_start)
+        outputs = loss - bsde - self.rho_optimal * self.T + self.value_optimal(X_now)\
+        - self.value_optimal(X_start)
         control_optimal = keras.Model(inputs=inputs, outputs = outputs, name = 'control_optimal')
         self.optnn = control_optimal
         
@@ -79,7 +154,7 @@ class BSDE:
             inputs = inputs + [input_dW]
             u_now = self.unn(X_now)
         #     u_now = -0.5*d_value_NN(X_now)    #  Connect u with dV/dx 
-            X_next  = X_now + input_dW + u_now * self.dt
+            X_next  = X_now + input_dW + self.dynamic(u_now,X_now) * self.dt
             loss_tmp = (tf.math.square(X_now) + tf.math.square(u_now))*self.dt
             loss_output = loss_output + [loss_tmp]
             loss = loss + loss_tmp
@@ -96,10 +171,10 @@ class BSDE:
     def plot_compare(self):
         NN_plot(self.Vnn)
         xgrid = np.linspace(-10,10,201)
-        plt.plot(xgrid, xgrid**2)
+        plt.plot(xgrid, self.value_optimal(xgrid))
         plt.show()
         NN_plot(self.unn)
-        plt.plot(xgrid, -xgrid)
+        plt.plot(xgrid, self.u_optimal(xgrid))
         plt.show()
         tmp = self.rhonn.predict(np.zeros(1))[0,0]
         print('Optimal ergodic cost is: ',tmp)
